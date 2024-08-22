247d1980f50963430203273b3afe6d6d3b4be84d
rennsoterio <renn.soterio@standards.org.au>
2024-08-23 07:27:51 +0800

    Create CQRS abstractions

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+{
+
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResponse>;
+public record CreateProductResponse(Guid Id);
+public class CreateProductHandler : IRequestHandler<CreateProductRequest, CreateProductResponse>
+{
+    public Task<CreateProductResponse> Handle(CreateProductRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
