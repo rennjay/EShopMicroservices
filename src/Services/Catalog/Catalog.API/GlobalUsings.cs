59790d8baac1b452b8dc826a46c40244ef67cce5
rennsoterio <renn.soterio@standards.org.au>
2024-08-26 21:41:44 +0800

    Add Get products endpoints

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using Carter;
 global using Mapster;
 global using MediatR;
 global using Marten;
+global using Catalog.API.Exceptions;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdResponse(Product Product);
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return TypedResults.Ok(response);
+            
+        })
+        .WithName("GetProductById")
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product by ID")
+        .WithDescription("Get Product by ID");
+    }
+}
