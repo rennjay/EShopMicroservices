ceadb8f335d11fd0c2f3ab5b2d67295a4aa6b52c
Rennjay Soterio <rennjaysoterio@gmail.com>
2024-09-23 08:55:50 +0800

    Add Carter endpoints and MediatR integration
    Updated Basket.API.csproj to include Carter and MediatR packages, target .NET 8.0, and enable nullable and implicit usings. Configured Program.cs to use MediatR and Carter, and mapped Carter endpoints. Added global using directives in GlobalUsings.cs. Implemented GetBasketEndpoint for retrieving a basket by username and GetBasketQueryHandler with a hardcoded response.

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+internal class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        // todo: Get from db
+
+        return Task.FromResult(new GetBasketResult(new ShoppingCart("renn")));
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+{
+    var targetAssembly = typeof(Program).Assembly;
+    builder.Services.AddMediatR(opt =>
+    {
+        opt.RegisterServicesFromAssembly(targetAssembly);
+    });
+    builder.Services.AddCarter();
+}
+
 var app = builder.Build();
 
+app.MapCarter();
 app.Run();
