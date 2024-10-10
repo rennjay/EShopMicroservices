785cc98c361d3b0350c943657b6e84c93ee94955
Rennjay Soterio <rennjaysoterio@gmail.com>
2024-10-11 06:04:14 +0800

    Add Entity abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Abstractions;
+public abstract class Entity<TId> : IEntity<TId>
+{
+    public required TId Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
